35b526011862d6425b08462282a43cddf19e8cde
mertayaar <mertayaar@gmail.com>
2024-01-02 15:20:01 -0500

    layout and partials

---
+++ Frontend/HotelProject.WebUI/Controllers/AdminLayoutController.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult _AdminLayout()
+        {
+            return View();
+        }
+
+        public PartialViewResult HeadPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult ScriptPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PreloaderPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult NavHeaderPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult HeaderPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult SidebarPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult FooterPartial()
+        {
+            return PartialView();
+        }
+    }
+}
