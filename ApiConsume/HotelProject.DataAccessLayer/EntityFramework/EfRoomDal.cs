d3c71a655791e4de9389d0edaabd257e552e72d1
mertayaar <mertayaar@gmail.com>
2024-01-02 12:09:54 -0500

    entityframework classes

---
+++ ApiConsume/HotelProject.DataAccessLayer/EntityFramework/EfRoomDal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concrete;
+using HotelProject.DataAccessLayer.Repositories;
+using HotelProject.EntityLayer.Concrete;
+
+namespace HotelProject.DataAccessLayer.EntityFramework
+{
+	public class EfRoomDal : GenericRepository<Room>, IRoomDal
+	{
+		public EfRoomDal(Context context) : base(context)
+		{
+
+		}
+	}
+}
+
