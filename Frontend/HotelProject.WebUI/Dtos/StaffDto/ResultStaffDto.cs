8bfe271de82ba32f097dacce7dcc5c173280ed1b
mertayaar <mertayaar@gmail.com>
2024-01-04 00:44:00 -0500

    dynamic staff partial

---
+++ Frontend/HotelProject.WebUI/Dtos/StaffDto/ResultStaffDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace HotelProject.WebUI.Dtos.StaffDto
+{
+	public class ResultStaffDto
+	{
+        public required int StaffID { get; set; }
+
+        public required string Name { get; set; }
+
+        public required string Title { get; set; }
+
+        public required string Image { get; set; }
+
+        public required string SocialMedia1 { get; set; }
+
+        public required string SocialMedia2 { get; set; }
+
+        public required string SocialMedia3 { get; set; }
+    }
+}
+
--- Frontend/HotelProject.WebUI/ViewComponents/Default/_TeamPartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Default/_TeamPartial.cs
@@ -1,13 +1,34 @@
 ﻿using System;
+using HotelProject.WebUI.Dtos.StaffDto;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelProject.WebUI.ViewComponents.Default
 {
 	public class _TeamPartial : ViewComponent
 	{
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _TeamPartial(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            using (var client = _httpClientFactory.CreateClient())
+            {
+                var response = await client.GetAsync("http://localhost:5009/api/staff");
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonData = await response.Content.ReadAsStringAsync();
+                    var values = JsonConvert.DeserializeObject<List<ResultStaffDto>>(jsonData);
+
+                    return View(values);
+                }
+
+                return View();
+            }
         }
     }
 }
