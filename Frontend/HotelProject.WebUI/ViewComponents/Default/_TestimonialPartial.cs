4f6a361d0b3eed685a5332be9738e57a3b89259e
mertayaar <mertayaar@gmail.com>
2024-01-04 00:35:39 -0500

    dynamic testimonial partial

--- Frontend/HotelProject.WebUI/ViewComponents/Default/_TestimonialPartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Default/_TestimonialPartial.cs
@@ -1,13 +1,34 @@
 ﻿using System;
+using HotelProject.WebUI.Dtos.TestimonialDto;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelProject.WebUI.ViewComponents.Default
 {
 	public class _TestimonialPartial : ViewComponent
 	{
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _TestimonialPartial(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            using (var client = _httpClientFactory.CreateClient())
+            {
+                var response = await client.GetAsync("http://localhost:5009/api/testimonial");
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonData = await response.Content.ReadAsStringAsync();
+                    var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+
+                    return View(values);
+                }
+
+                return View();
+            }
         }
     }
 }
